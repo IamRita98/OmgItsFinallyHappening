1ac1823d084ad3231bf3e703571aba9134114d00
Rita <youarereta@gmail.com>
2026-04-09 22:06:57 -0400

    LERPING

--- Assets/_Scripts/GridMovement.cs
+++ Assets/_Scripts/GridMovement.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 public class GridMovement : MonoBehaviour
 {
     Vector3 gridSize;
+    public float lerpTime;
+    float lerpTimer;
+    bool playerWantsToMove = false;
     public bool inCombat = false;
     UnitSelector unitSelector;
     public bool canMove = true;
     public Vector2 currentPos;
+    Vector2 newPos;
 
     private void Awake()
     {
@@ -17,34 +21,75 @@ public class GridMovement : MonoBehaviour
     void Update()
     {
         if (!canMove) return;
-        Move();
+        if (playerWantsToMove) LerpMovement();
+        else Move();
     }
     void Move()
     {
         currentPos = transform.position;
-        Vector2 newPos = transform.position;
+
         if (!inCombat)
         {
-            if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W)) newPos = currentPos + Vector2.up * gridSize;
-            if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S)) newPos = currentPos + Vector2.down * gridSize;
-            if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D)) newPos = currentPos + Vector2.right * gridSize;
-            if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A)) newPos = currentPos + Vector2.left * gridSize;
-            
+            if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+            {
+                newPos = currentPos + Vector2.up * gridSize;
+                playerWantsToMove = true;
+            }
+            if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+            {
+                newPos = currentPos + Vector2.down * gridSize;
+                playerWantsToMove = true;
+            }
+            if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+            {
+                newPos = currentPos + Vector2.right * gridSize;
+                playerWantsToMove = true;
+            }
+            if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+            {
+                newPos = currentPos + Vector2.left * gridSize;
+                playerWantsToMove = true;
+            }
         }
         else
         {
             bool valid=unitSelector.CheckForMovement();
             if (valid)
             {
-                if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W)) newPos = currentPos + Vector2.up * gridSize;
-                if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S)) newPos = currentPos + Vector2.down * gridSize;
-                if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D)) newPos = currentPos + Vector2.right * gridSize;
-                if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A)) newPos = currentPos + Vector2.left * gridSize;
+                if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+                {
+                    newPos = currentPos + Vector2.up * gridSize;
+                    playerWantsToMove = true;
+                }
+                if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+                {
+                    newPos = currentPos + Vector2.down * gridSize;
+                    playerWantsToMove = true;
+                }
+                if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+                {
+                    newPos = currentPos + Vector2.right * gridSize;
+                    playerWantsToMove = true;
+                }
+                if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+                {
+                    newPos = currentPos + Vector2.left * gridSize;
+                    playerWantsToMove = true;
+                }
             }
-            
         }
-           
+    }
 
-        gameObject.transform.position = newPos;
+    void LerpMovement()
+    {
+        lerpTimer += Time.deltaTime;
+
+        float percent = lerpTimer / lerpTime;
+        if (lerpTimer > lerpTime)
+        {
+            lerpTimer = 0;
+            playerWantsToMove = false;
+        }
+        transform.position = Vector2.Lerp(currentPos, newPos, percent);
     }
 }
