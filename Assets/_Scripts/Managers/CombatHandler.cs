06e288451762a0c60eeb241f6dc50ce9d546dda1
Rita <youarereta@gmail.com>
2026-04-11 15:24:08 -0400

    Added combat calculations

--- Assets/_Scripts/Managers/CombatHandler.cs
+++ Assets/_Scripts/Managers/CombatHandler.cs
@@ -17,18 +17,23 @@ public class CombatHandler : MonoBehaviour
     public List<GameObject> allEnemiesList = new List<GameObject>();
     bool inCombat = false;
     int index = 0;
+    DrawTiles drawTiles;
+    UnitStatSheet unitStats;
+    UIManager uiManager;
 
     private void Awake()
     {
         allEnemiesList = GameObject.FindGameObjectsWithTag("Enemy").ToList();
         unitSelectorGO = GameObject.FindGameObjectWithTag("UnitSelector");
+        drawTiles = gameObject.GetComponent<DrawTiles>();
+        uiManager = GameObject.FindWithTag("UIManager").GetComponent<UIManager>();
     }
     public void AttackSelected()
     {
         playerObject = unitSelectorGO.GetComponent<UnitSelector>().PlayerGO;
         //enemyObject = GameObject.FindGameObjectWithTag("UnitSelector").GetComponent<UnitSelector>().GOHovered;
-        DrawTiles drawTiles = gameObject.GetComponent<DrawTiles>();
-        UnitStatSheet unitStats = playerObject.GetComponent<UnitStatSheet>();
+
+        unitStats = playerObject.GetComponent<UnitStatSheet>();
         unitStats.GetAttackRange();
         drawTiles.DrawTilesGO(AttackTiles, unitStats.attackTiles);
         attackRange = unitStats.attackTiles;
@@ -36,10 +41,9 @@ public class CombatHandler : MonoBehaviour
         {
             foreach (var enemy in allEnemiesList)
             {
-                Vector2 temp=new Vector2(enemy.transform.position.x, enemy.transform.position.y);
-                if (temp==tile) enemiesToAttack.Add(enemy);
+                Vector2 temp = new Vector2(enemy.transform.position.x, enemy.transform.position.y);
+                if (temp == tile) enemiesToAttack.Add(enemy);
             }
-            
         }
         if (enemiesToAttack.Count > 0)
         {
@@ -47,11 +51,11 @@ public class CombatHandler : MonoBehaviour
             Vector2 tempPos = enemiesToAttack[index].transform.position;
             unitSelectorGO.transform.position = tempPos;
         }//do something else if no enemies
-        
+
     }
     private void Update()
     {
-        if(inCombat)
+        if (inCombat)
         {
             if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
             {
@@ -60,8 +64,8 @@ public class CombatHandler : MonoBehaviour
                 {
                     index = enemiesToAttack.Count - 1;
                 }
-                    Vector2 tempPos = enemiesToAttack[index].transform.position;
-                    unitSelectorGO.transform.position = tempPos;
+                Vector2 tempPos = enemiesToAttack[index].transform.position;
+                unitSelectorGO.transform.position = tempPos;
             }
             if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
             {
@@ -70,9 +74,14 @@ public class CombatHandler : MonoBehaviour
                 {
                     index = 0;
                 }
-                    Vector2 tempPos = enemiesToAttack[index].transform.position;
-                    unitSelectorGO.transform.position = tempPos;
+                Vector2 tempPos = enemiesToAttack[index].transform.position;
+                unitSelectorGO.transform.position = tempPos;
             }
+            UnitStatSheet enemyStats = enemiesToAttack[index].GetComponent<UnitStatSheet>();
+            CombatCalc(unitStats, enemyStats);
+            //CombatCalc()
+            //UI.ShowCombatCalcs()
+
             if (Input.GetKeyDown(KeyCode.Z))
             {
                 //Confirm Attack
@@ -82,7 +91,24 @@ public class CombatHandler : MonoBehaviour
                 //Cancel Attack
                 inCombat = false;
                 enemiesToAttack.Clear();
+                drawTiles.ClearTiles();
+                uiManager.HideCombatCalcs();
             }
         }
+    }
+
+    public void CombatCalc(UnitStatSheet attackerStats, UnitStatSheet defenderStats)
+    {
+        int attackerHp = (int)(attackerStats.Health.Value);
+        int attackerDamage = (int)Mathf.Clamp((attackerStats.Strength.Value - defenderStats.Defense.Value), 1, Mathf.Infinity);
+        int attackerHitChance = (int)(attackerStats.HitChance.Value + attackerStats.Skill.Value - defenderStats.Speed.Value);
+        int attackerCritChance = (int)(attackerStats.Skill.Value);
+
+        int defenderHp = (int)(defenderStats.Health.Value);
+        int defenderDamage = (int)Mathf.Clamp((defenderStats.Strength.Value - attackerStats.Defense.Value), 1, Mathf.Infinity);
+        int defenderHitChance = (int)(defenderStats.HitChance.Value + defenderStats.Skill.Value - attackerStats.Speed.Value);
+        int defenderCritChance = (int)(defenderStats.Skill.Value);
+        //if(isPlayersTurn)
+        uiManager.ShowCombatCalcs(attackerHp, defenderHp, attackerDamage, defenderDamage, attackerHitChance, defenderHitChance, attackerCritChance, defenderCritChance);
     }
 }
--- Assets/_Scripts/Managers/UIManager.cs
+++ Assets/_Scripts/Managers/UIManager.cs
@@ -1,13 +1,24 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using TMPro;
 
 public class UIManager : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public GameObject combatOptions;
+    public GameObject combatCalcUI;
     public Button attackButton;
     Selectable firstSelected;
+
+    public TMP_Text playerHPText;
+    public TMP_Text playerDamageText;
+    public TMP_Text playerHitChanceText;
+    public TMP_Text playerCritChanceText;
+    public TMP_Text enemyHPText;
+    public TMP_Text enemyDamageText;
+    public TMP_Text enemyHitChanceText;
+    public TMP_Text enemyCritChanceText;
 
     public void EnableCombatUI()
     {
@@ -20,6 +31,23 @@ public class UIManager : MonoBehaviour
     {
         combatOptions.SetActive(false);
     }
-    // Update is called once per frame
-   
+
+    public void ShowCombatCalcs(int playerHP, int enemyHP, int playerDamage, int enemyDamage, int playerHit, int enemyHit, int playerCrit, int enemyCrit)
+    {
+        combatCalcUI.SetActive(true); 
+        playerHPText.text = playerHP.ToString();
+        playerDamageText.text = playerDamage.ToString();
+        playerHitChanceText.text = playerHit.ToString();
+        playerCritChanceText.text = playerCrit.ToString();
+
+        enemyHPText.text = enemyHP.ToString();
+        enemyDamageText.text = enemyDamage.ToString();
+        enemyHitChanceText.text = enemyHit.ToString();
+        enemyCritChanceText.text = playerCrit.ToString();
+    }
+
+    public void HideCombatCalcs()
+    {
+        combatCalcUI.SetActive(false);
+    }
 }
