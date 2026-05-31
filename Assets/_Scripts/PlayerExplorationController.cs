3d2005e21b41a528db3558944ed5531663a5296a
Lashvir <dimagax01@gmail.com>
2026-05-31 14:10:26 -0400

    Added free movement when in exploration mode, need to work on raycasting

--- Assets/_Scripts/PlayerExplorationController.cs
+++ Assets/_Scripts/PlayerExplorationController.cs
@@ -1,4 +1,6 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerExplorationController : MonoBehaviour
 {
@@ -6,9 +8,10 @@ public class PlayerExplorationController : MonoBehaviour
     public Sprite playerExplorationSprite;
     public Vector2 rayDir;
     GridMovement gM;
-    float maxRayDist = 2f;
+    float maxRayDist = 1.3f;
     RaycastHit2D ray;
     int mask;
+    FreeMovement fM;
     private void Start()
     {
         //playerSRend = gameObject.GetComponent<SpriteRenderer>();
@@ -18,6 +21,8 @@ public class PlayerExplorationController : MonoBehaviour
     {
         playerSRend = gameObject.GetComponent<SpriteRenderer>();
         gM = gameObject.GetComponent<GridMovement>();
+        fM = gameObject.GetComponent<FreeMovement>();
+        gM.enabled=false;
     }
     private void Update()
     {
@@ -29,10 +34,29 @@ public class PlayerExplorationController : MonoBehaviour
     }
     void CheckRayCast()
     {
-        CheckDirFacing();
+        //CheckDirFacing();
+        NattyDir();
         ray = Physics2D.Raycast(transform.position, rayDir,maxRayDist,mask);
+        Quaternion dir2=transform.rotation = new Quaternion(gameObject.transform.rotation.x, transform.rotation.y, transform.rotation.z+15,0);
+        Vector3 test = transform.position;
+        transform.eulerAngles = new Vector3(0, 0, 15);
+        //RaycastHit2D ray2 = Physics2D.Raycast(transform.rotation.z+15, rayDir, maxRayDist, mask);
+        Quaternion dir3 = transform.rotation = new Quaternion(gameObject.transform.rotation.x, transform.rotation.y, transform.rotation.z - 15, 0);
         CheckRayHits();
         //check enum state and set direction
+    }
+    private void NattyDir()
+    {
+        if (fM.dir == Vector2.zero)
+        {
+            //maintain last dir
+        }
+        else {
+            rayDir = fM.dir;
+            
+        }
+        Debug.DrawRay(transform.position, rayDir * maxRayDist, Color.green, 1f);
+
     }
     private void CheckRayHits()
     { 
---
+++ Assets/_Scripts/Utility/FreeMovement.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class FreeMovement : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+    public float speed = 1f;
+    public Vector2 dir;
+    public DirectionFacing dirFacing;
+
+    private void OnEnable()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = dir * speed;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+        RayCastDir();
+    }
+    private void RayCastDir()
+    {
+
+    }
+    private void Movement()
+    {
+        dir.x = Input.GetAxisRaw("Horizontal");
+        dir.y = Input.GetAxisRaw("Vertical");
+        dir.Normalize();
+    }
+}
