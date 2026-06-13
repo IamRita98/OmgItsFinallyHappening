19820f2660240d1bb43c5c14720d69b99cefaa0a
Rita <youarereta@gmail.com>
2026-06-13 14:27:00 -0400

    This is wack

--- Assets/_Scripts/Items/ConsumableItem.cs
+++ Assets/_Scripts/Items/ConsumableItem.cs
@@ -1,13 +1,22 @@
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "ConsumableItem", menuName = "Items/ConsumableItem")]
-public class ConsumableItem : Item
+public class ConsumableItem : Item, IUsable, IEquipable
 {
     //are gonna affect stats
     public Rarity rarity;
 
+    public void Equip()
+    {
+        throw new System.NotImplementedException();
+    }
 
-    public void DoSomething()
+    public void UnEquip()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Use()
     {
         Debug.Log("hello");
     }
